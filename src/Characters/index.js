2dabf1eb51b45b454149a2806d75ea6d3b785c5a
Michele <michelecocuccio@gmail.com>
2021-10-07 22:36:22 +0200

    Finishes solution part 1

--- src/Characters/index.js
+++ src/Characters/index.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Grid } from "@mui/material";
 import Character from "./Character";
+import axios from "axios";
 
-export default function Characters() {
+export default function Characters({ page }) {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      const url = `https://rickandmortyapi.com/api/character?page=${page}`;
+      const response = await axios.get(url);
+      response.status === 200 && setCharacters(response.data.results);
+    }
+
+    fetchCharacters();
+  }, [page]);
+
   return (
     <Container fixed sx={{ mt: 4 }}>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <Character />
-        </Grid>
-        <Grid item xs={3}>
-          <Character />
-        </Grid>
-        <Grid item xs={3}>
-          <Character />
-        </Grid>
-        <Grid item xs={3}>
-          <Character />
-        </Grid>
+        {characters?.map((character) => (
+          <Grid item xs={3} key={character.id}>
+            <Character character={character} />
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
